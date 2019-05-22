2d40e14d2f5dd632ae5473ffe7285805c79aaddc
Zymryte <zymryteeek@gmail.com>
2019-05-22 12:29:06 +0200

    Kompletimi i pikes se 3te

--- Php/Form.php
+++ Php/Form.php
@@ -2,17 +2,29 @@
 <?php
 
 $reg=$_GET['Comments'];
-$arr=explode('',$reg);
-echo "Your regard was: ". "<br>";
+$arr=explode(' ',$reg);
+echo "<br>Your regard was: ";
+$lengthi = "";
 foreach($arr as $words){
     echo $words. ",";
+    $lengthi = $lengthi . $words ;
 }
-
+echo "You have written ".strlen("$lengthi")." letters<br>";
 
 $reg=implode('', $arr);
-echo "Your regard was : $reg";
-
-$str = "Thank you for your regard!";
+echo "<br>Your regard was : $reg<br>";
+$str = "Thank you for your regard!" ."<br>";
 echo $str . "<br>";
-echo trim($str,"Thd!");
+echo trim($str,"Thd!") . "<br>";
+
+$number = 500;
+$str = "ModernSchool" ;
+printf("There are %u students in %s ",$number,$str)."<br>";
+echo "<br>";
+echo substr("Apply now!",6)."<br>";
+
+
+$arr = array("blue","red","green","yellow");
+print_r(str_replace("red","pink",$arr,$i));
+echo "<br>" . "Replacements: $i" ." <br>";
 ?>
